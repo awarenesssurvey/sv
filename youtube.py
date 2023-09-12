0c8325ef10b2704cb45f8b2be5fb5fe620198368
data-manipulation-process <jagadusu01@gmail.com>
2023-09-12 22:28:10 +0100

    readme file

--- youtube.py
+++ youtube.py
@@ -1,8 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 # YouTube video URL or video ID
-# video_url = "https://www.youtube.com/watch?v=7RWgy8YoXJ4"
-video_url =  "https://www.youtube.com/watch?v=WbzJgPJ8bos"
+video_url = "https://www.youtube.com/watch?v=7RWgy8YoXJ4"
 
 # Function to get the transcript
 def get_transcript(video_url):
@@ -13,12 +12,12 @@ def get_transcript(video_url):
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
         # Save the transcript to a text file
-        with open("Transcript/transcript.txt", "w", encoding="utf-8") as file:
+        with open("Transcript/transcript.docx", "w", encoding="utf-8") as file:
             for entry in transcript:
                 text = entry['text']
                 file.write(text + "\n")
 
-        print("Transcript downloaded and saved as 'transcript.txt'.")
+        print("Transcript downloaded and saved as 'transcript.docx'.")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
