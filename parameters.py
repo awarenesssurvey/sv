8d3a7e2b19c7dfeeb166b695743692dd2fe77047
data-manipulation-process <jagadusu01@gmail.com>
2023-09-12 23:26:25 +0100

    readme file

---
+++ parameters.py
@@ -0,0 +1,91 @@
+
+q1_gender_list = [
+    "Male",
+    "Female",
+    "Non-binary",
+    "Prefer not to say"
+]
+
+q2_grades_list = [
+    "9th grade",
+    "10th grade",
+    "11th grade",
+    "12th grade"
+]
+
+q3_age_groups = [
+    "Under 15",
+    "15-16",
+    "16-17",
+    "Over 18"
+]
+
+q4_ethnicities = [
+    "White",
+    "Black or African American",
+    "Hispanic or Latino",
+    "Asian",
+    "Native American",
+    "Two or more races"
+]
+
+q5_heard_of_options = [
+    "Yes",
+    "No",
+    "Maybe"
+]
+
+q6_knowledge_levels = [
+    "No Knowledge",
+    "Limited Knowledge",
+    "Moderate Knowledge",
+    "Very Knowledgeable"
+]
+
+q7_symptoms = [
+    "Irregular periods",
+    "Excess facial or body hair growth",
+    "Weight gain or difficulty losing weight",
+    "Acne",
+    "Thinning hair or hair loss",
+    "Dark patches of skin",
+    "Multiple cysts on the ovaries"
+]
+
+q8_pcos_symptoms = [
+    "Yes",
+    "No"
+]
+
+q9_pcos_support_responses = [
+    "Yes, I would be very supportive",
+    "I would try to be supportive but don't know much about PCOS",
+    "I don't think I would know how to support them",
+    "No, I would not be very supportive"
+]
+
+q10_information_sources = [
+    "Doctor",
+    "School Nurse",
+    "Health teacher",
+    "Parents",
+    "Friends",
+    "Online Resources",
+    "Books/Magazines",
+    "I would not seek more information"
+]
+
+q11_treatment_options = [
+    "Birth control/contraceptive pills",
+    "Diabetes medication",
+    "Hair removal creams/laser",
+    "Healthy diet",
+    "Exercise",
+    "Surgery",
+    "Don't know"
+]
+
+q12_survey_responses = [
+    "Yes",
+    "No"
+]
